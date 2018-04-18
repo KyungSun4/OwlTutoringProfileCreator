053d3931e8bdf579d3208a68c20a46cf3977b75c
KyungSun4 <john.han7@yahoo.com>
2018-04-18 17:27:35 -0500

    minor aesthetic fixes

--- finance_function_generator.py
+++ finance_function_generator.py
@@ -6,7 +6,7 @@ Creates function calls with list of tutor spreadsheets inputs. Whenever a new tu
 their name can be added to the csv list and the new functions can be created that include their
 name.
 
-Example function call:
+Example outputs:
 =INCOME({'Name1'!A$11:A, 'Name2'!A$11:A},{'Name1'!B$11:B, 'Name2'!B$11:B}, TutorList!B$3:B$NUMBER_OF_TUTORS_PLUS_2,{'Name1'!E$11:E, 'Name2'!E$11:E},A3, B3)
 
 =PAIDOUT({'Name1'!A$11:A, 'Name2'!A$11:A},{'Name1'!B$11:B, 'Name2'!B$11:B}, TutorList!B$3:B$NUMBER_OF_TUTORS_PLUS_2,{'Name1'!E$11:E, 'Name2'!E$11:E},{'Name1'!F$11:F,'Name2'!F$11:F},A3, B3)
@@ -27,7 +27,6 @@ def create_name_list(names, before, after, divider):
 		string += before + name + after
 		if index != (len(names)-1):
 			string += divider
-
 	return string
 
 # Get names from csv file
@@ -37,10 +36,10 @@ with open('tutors.csv', 'r') as csvfile:
 		names.append(row[0])
 		print(row[0])
 
-# Create output string
-
+# Create output strings
 total_in_string = "=INCOME"
 total_out_string = "=PAIDOUT"
+
 shared_string = "({"
 shared_string += create_name_list(names, "'", "'!A$11:A", ", ")
 shared_string += "},{"
@@ -57,17 +56,19 @@ total_out_string +=",TutorList!C$3:C$" + str(len(names)+2)
 total_out_string += ",{"
 total_out_string += create_name_list(names, "'", "'!E$11:E", ", ")
 total_out_string += "},{"
-total_out_string +=create_name_list(names, "'", "'!F$11:F", ", ")
+total_out_string += create_name_list(names, "'", "'!F$11:F", ", ")
 total_out_string += "},A3, B3)"
 
-# Output string to terminal
+# Output strings to terminal and save to files
 print("\n\nTotal In function:\n\n")
 print(total_in_string)
+
 with open('total_in.txt', 'w') as file:
     file.write(total_in_string)
 
 print("\n\nTotal out function:\n\n")
 print(total_out_string)
+
 with open('total_out.txt', 'w') as file:
     file.write(total_out_string)
 
